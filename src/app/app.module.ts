58ff0b3a887d449ca03ef31c215c526e9b85bed5
Nita Mahajan <nita.mahajan@exagroup.biz>
2024-04-04 12:12:24 +0530

    Nesting other compoennts in appComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-
+import { ServerComponent } from './server/server.component';
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [ // Which components this module will use, or "packages"
+    AppComponent,
+    ServerComponent
   ],
-  imports: [
+  imports: [ // Which modules this module will use
     BrowserModule,
     FormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent] // Which component to render at selector specified in component
 })
 export class AppModule { }
---
+++ src/app/server/server.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'server-component',
+    templateUrl: './server.component.html',
+    styleUrl: './server.component.css'
+})
+
+export class ServerComponent {}
