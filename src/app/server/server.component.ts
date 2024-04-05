477bb4c281402b31a46bf3f7366a61a6c463bb36
Nita Mahajan <nita.mahajan@exagroup.biz>
2024-04-05 19:01:25 +0530

    property binding in angular

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,9 +1,22 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: '[server-component]', // selecting by attribute
+    selector: 'server-component', // selecting by attribute
     templateUrl: './server.component.html',
     styleUrl: './server.component.css'
 })
 
-export class ServerComponent {}
+export class ServerComponent {
+    disableToAddServer = true;
+
+    constructor() {
+        setTimeout(() => {
+            this.disableToAddServer = false;
+        }, 2000)
+    }
+    serverId: number = 2;
+    serverStatus: string = 'running';
+    getServerStatus() {
+        return this.serverStatus;
+    }
+}
